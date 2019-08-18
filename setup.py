1887899a859660f423b8408a167a331e8438ee7b
fasborn <fasborn@gmail.com>
2019-08-18 18:33:46 +0300

    Bugfix!

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(name="tprocessing",
       version="1.0",
-      packages=["tprocessing"])
+      packages=["tprocessing"],
+      url="https://github.com/fasborn/tprocessing",
+      author="Fasborn",
+      long_description=README,
+      long_description_content_type = "text/markdown")
